2a49dc065b0f45e6c1edd0bcf5a23367c9980940
seongmin-01 <fafa5271783@gmail.com>
2025-03-25 13:31:20 +0900

    commit 0.0.1

---
+++ src/test/java/kr/co/ksm/SpringGithubActionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ksm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
